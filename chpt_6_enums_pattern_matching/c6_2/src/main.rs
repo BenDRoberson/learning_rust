29ad1a69c2f7b438e76f928560556d736300c5a2
Ben Roberson <bendroberson@gmail.com>
2024-04-19 19:47:24 -0400

    Chapter 6.2: Done - cool section!

--- chpt_6_enums_pattern_matching/c6_2/src/main.rs
+++ chpt_6_enums_pattern_matching/c6_2/src/main.rs
@@ -1,19 +1,58 @@
 fn main() {
-    println!("Hello, world!");
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
 
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
+}
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => { //note {} not required unless > 1 line
+            println!("Lucky penny!");
+            1
+        }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
+
+// we can use matching with 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+// we must list out every case (for example removing 'None' above will cause compiler to yell)
+// what if we had 100+ values to match? how can we avoid only calling out a few cases?
+match dice_roll {
+    3 => add_fancy_hat(),
+    7 => remove_fancy_hat(),
+    other => move_player(other),
+}
+
+// OR if we don't plan to use it
+match dice_roll {
+    3 => add_fancy_hat(),
+    7 => remove_fancy_hat(),
+    _ => reroll(),
+}
+
